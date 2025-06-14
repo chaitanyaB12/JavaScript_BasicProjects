01e628a6e17b686ac26099e5081fe6cafa30bf82
Chaitanya <chaitanyabhopnikar@gmail.com>
2025-06-14 20:24:50 +0530

    JS_Projects Commit

---
+++ 5-unlimitedColors/color.js
@@ -0,0 +1,30 @@
+const randomcolor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+      } 
+        return color; 
+    };
+    let intervalId;
+    const startchangingcolor = function(){
+        if(!intervalId){
+        intervalId = setInterval(changBgcolor, 1000);
+        }
+
+         function changBgcolor(){
+         document.body.style.backgroundColor = randomcolor();
+        } 
+         };
+
+    
+    
+    const stopchangingcolor = function(){
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+
+    document.querySelector('#start').addEventListener('click', startchangingcolor);
+    document.querySelector('#stop').addEventListener('click', stopchangingcolor); 
+
+
